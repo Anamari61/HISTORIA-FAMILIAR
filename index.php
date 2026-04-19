980c6aee367aa95ea1372d89f0e88c503f994f8a
Anamari61 <aruano@uoc.edu>
2026-04-19 11:14:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php include("includes/awb_init.php"); ?>
+<!DOCTYPE html > 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es" > 
+<head> 
+<title>Genealog&iacute;a RUANO-BEN&Iacute;TEZ</title> 
+<meta charset="utf-8"> 
+<meta name="description" content="Genealog&iacute;a RUANO-BEN&Iacute;TEZ " /> 
+<meta name="keywords" content="Généalogie, Genealogy, ancestors, ancêtres, descendance, famille, family tree, fredwebbook" /> 
+<meta name="author" content="ANA MAR&Iacute;A RUANO BEN&Iacute;TEZ" /> 
+<meta name="generator" content="Ancestris" /> 
+<meta name="robots" content="all" /> 
+<meta name="reply-to" content="ARUANO@UOC.EDU" /> 
+<meta name="owner" content="ANA MAR&Iacute;A RUANO BEN&Iacute;TEZ" /> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+
+<link rel="stylesheet" href="theme/style.css" type="text/css"/>
+</head>
+<body>
+
+<!-- START OF VARIABLE CONTENT -->
+
+<div class="title"><a id="top">&nbsp;</a>Genealog&iacute;a RUANO-BEN&Iacute;TEZ</div>
+<hr />
+<div class="contindex">
+<div class="menu">
+<p>Individuos</p>
+<ul>
+<li><a href="names/names.php">Apellidos</a></li>
+<li><a href="persons/persons_1.php">Lista de los individuos</a></li>
+<li><a href="details/personsdetails_1.php">Detalles de los individuos</a></li>
+</ul>
+<p>Documentos</p>
+<ul>
+<li><a href="sources/sources_0.php">Fuentes</a></li>
+<li><a href="media/media_0.php">Fotos y otros multimedia</a></li>
+</ul>
+<p>Lugares</p>
+<ul>
+<li><a href="map/map.php">Mapa geogr&aacute;fico</a></li>
+<li><a href="cities/cities.php">Lista de ciudades</a></li>
+<li><a href="citiesdetails/citiesdetails_1.php">Detalles de ciudades</a></li>
+</ul>
+<p>Calendario familiar</p>
+<ul>
+<li><a href="days/days.php">Calendario familiar</a></li>
+<li><a href="daysdetails/daysdetails_1.php">Detalles del calendario</a></li>
+</ul>
+<p>Estad&iacute;sticas</p>
+<ul>
+<li><a href="stats/stats_1.php">Estad&iacute;sticas</a></li>
+<li><a href="stats/stats_2.php">Grado de endogamia</a></li>
+</ul>
+<p>Lista estructurada</p>
+<ul>
+<li><a href="repsosa/repsosa_1.php">Lista de ancestros</a></li>
+</ul>
+<p>Herramientas</p>
+<ul>
+<li><a href="search/search_0.php">B&uacute;squeda de individuos</a></li>
+</ul></div>
+<div class="intro">
+<h3>
+ÁRBOL GENEALÓGICO EN CONSTRUCCIÓN
+</h3>
+Agradezco vuestras aportaciones: nombres, fechas, datos...
+<br /><br /><hr /><br />
+El primer individuo de esta genealogía es <img src='theme/f.gif' alt='Mujer' />&nbsp;<a href='details/personsdetails_4.php#I00001'>ANA MAR&Iacute;A RUANO BEN&Iacute;TEZ</a>&nbsp;(19 May 1961). Sus 76 antepasados han sido encontrados durante 6 generaciones.<br />
+El ancestro más antiguo de el/ella es <img src='theme/m.gif' alt='Hombre' />&nbsp;<a href='details/personsdetails_4.php#I00171'>JUAN RUANO CA&Ntilde;ETE</a>.<br />
+<br />
+El individuo con la línea más larga de antepasados es <img src='theme/m.gif' alt='Hombre' />&nbsp;<a href='details/personsdetails_2.php#I00003'>GERM&Aacute;N CHIVITE RUANO</a>&nbsp;(5 May 1993) con los ancestros en 7 generaciones.<br />
+Es la persona con el mayor número de antepasados.<br />
+<br /><hr /><br />
+El lugar más frecuente de esta genealogía MÁLAGA.
+&nbsp;<a href="map/map.php?CUEVAS_DE_SAN_MARCOS__MALAGA_______"><img src="theme/map.gif" alt="M&Aacute;LAGA" title="Mapa que apunta a la posici&oacute;n M&Aacute;LAGA"/></a>
+<br />
+Esta genealogía include 194 individuos y 66 familias representando 96 apellidos y 6 diferentes lugares.<br />
+Entre estos individuos, además del de-cujus individual 76 son antepasados directos, 57 son primos y 60 no son del mismo linaje.<br />
+Las 66 uniones incluyendo 60 los matrimonios han dado a luz a 2,13 hijos de promedio.<br />
+<br /><hr /><br />
+Autor:&nbsp;ANA MARÍA RUANO BENÍTEZ<br />
+<a href="mailto:ARUANO@UOC.EDU?subject=Mensaje%20acerca%20de%20su%20genealogía&amp;body=Querido/Querida%20ANA MARÍA RUANO BENÍTEZ,%0a%0aContacto%20con%20Ud%20porque ">Contacteme por correo electrónico</a><br />
+<br /><hr /><br />
+<p class="legal">Estas páginas han sido creadas por <a href="https://www.ancestris.org" title="13.0.13853" >Ancestris WebBook</a> sobre 19 abr 2026 a  10:54.</p>
+</div>
+<div class="spacer">&nbsp;</div>
+</div>
+<p><a id="bot"></a>&nbsp;</p>
+
+<!-- END OF VARIABLE CONTENT -->
+
+<?php include("includes/awb_footer.php"); ?>
